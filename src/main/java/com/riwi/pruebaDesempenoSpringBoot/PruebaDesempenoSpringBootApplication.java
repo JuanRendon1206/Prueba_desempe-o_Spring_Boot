d42e59e67c672a02f50bdcc11b3303dcd748fd29
JuanRendon1206 <juanjoseflorezrendon124@gmail.com>
2024-06-04 22:08:52 -0500

    First commit

---
+++ src/main/java/com/riwi/pruebaDesempenoSpringBoot/PruebaDesempenoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.pruebaDesempenoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaDesempenoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaDesempenoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/pruebaDesempenoSpringBoot/PruebaDesempenoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.pruebaDesempenoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaDesempenoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
